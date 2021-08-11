73534633ae3db53d30857c1d2c61697e47f1711f
Audrey <audrey.gillies@gmail.com>
2021-08-11 11:00:01 -0600

    variables delcared in js file accessing each element by id

---
+++ cocktailsPageAssets/cocktailScript.js
@@ -0,0 +1,48 @@
+//Accessing Sidebar Link Elements
+var sidebarLink1El = $("#sidebar-link1");
+var sidebarLink2El = $("#sidebar-link2");
+var sidebarLink3El = $("#sidebar-link3");
+
+//Accessing Main Popular Recipe Card Elements
+var mainPopularImgEl = $("#main-popular-img");
+var mainPopularTitleEl = $("#main-popular-title");
+var mainPopularList1El = $("#main-popular-list1");
+var mainPopularList2El = $("#main-popular-list2");
+var mainPopularLinkEl = $("#main-popular-recipe-link");
+
+//Accessing Popular Recipe Card Elements
+
+//Card 1
+var popular1ImgEl = $("#popular1-img");
+var popular1TitleEl = $("#popular1-title");
+var popular1List1El = $("#popular1-list1");
+var popular1List2El = $("#popular1-list2");
+var popular1LinkEl = $("#popular1-recipe-link");
+
+//Card 2
+var popular2ImgEl = $("#popular2-img");
+var popular2TitleEl = $("#popular2-title");
+var popular2List1El = $("#popular2-list1");
+var popular2List2El = $("#popular2-list2");
+var popular2LinkEl = $("#popular2-recipe-link");
+
+//Card 3
+var popular3ImgEl = $("#popular3-img");
+var popular3TitleEl = $("#popular3-title");
+var popular3List1El = $("#popular3-list1");
+var popular3List2El = $("#popular3-list2");
+var popular3LinkEl = $("#popular3-recipe-link");
+
+//Card 4
+var popular4ImgEl = $("#popular4-img");
+var popular4TitleEl = $("#popular4-title");
+var popular4List1El = $("#popular4-list1");
+var popular4List2El = $("#popular4-list2");
+var popular4LinkEl = $("#popular4-recipe-link");
+
+//Accessing Random Recipe Card Elements
+var randomImgEl = $("#random-img");
+var randomTitleEl = $("#random-title");
+var randomList1El = $("#random-list1");
+var randomList2El = $("#random-list2");
+var randomLinkEl = $("#random-recipe-link");
